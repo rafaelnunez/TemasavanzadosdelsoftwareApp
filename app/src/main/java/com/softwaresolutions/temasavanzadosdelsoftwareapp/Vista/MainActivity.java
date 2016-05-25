65d9d3e6e6bf14371ed216eedc919f69919fe421
rafaelnunez <1995nunezrafael@gmail.com>
2016-05-25 12:48:11 -0500

    commit #1

---
+++ app/src/main/java/com/softwaresolutions/temasavanzadosdelsoftwareapp/Vista/MainActivity.java
@@ -0,0 +1,15 @@
+package com.softwaresolutions.temasavanzadosdelsoftwareapp.Vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softwaresolutions.temasavanzadosdelsoftwareapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
